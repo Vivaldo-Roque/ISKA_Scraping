7742db329f1ed9e02237bd84231e0611c74ea95f
Vivaldo-Roque <2001vivaldo@gmail.com>
2022-07-11 19:25:23 +0100

    first commit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 from notas import PegarMinhasNotas
 
 # ISKA Secretaria suas credenciais de login
-username = "190210" # Seu nome de usuário
-password = "006272580LA048" # Sua palavra-passe
+username = "####" # Seu nome de usuário
+password = "####" # Sua palavra-passe
 
 # Função que pega as notas
 PegarMinhasNotas(username=username, password=password)
